7deceaa9650074b25fbdda4ff7ffd4c217b8bcc8
encersil7-dotcom <zayar400431025@gmail.com>
2026-03-06 17:40:16 +0700

    My first Python file

---
+++ hello.py
@@ -0,0 +1,20 @@
+
+name = "Zayar"
+age = 20
+print(name)
+
+a = 10
+b = 5
+print(a + b) # ပေါင်းခြင်း
+print(a - b) # နှုတ်ခြင်း
+print(a * b) # မြှောက်ခြင်း
+print(a / b) # စားခြင်း
+
+name = input("မင်းနာမည် ဘယ်သူလဲ? ")
+print("မင်္ဂလာပါ " + name)
+
+age = 18
+if age >= 18:
+    print("မင်းက လူကြီးဖြစ်ပြီ")
+else:
+    print("မင်းက ကလေးပဲ ရှိသေးတယ်")
